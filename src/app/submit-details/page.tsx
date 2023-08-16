a86b162de207f4ec4fbf70236586fbc71cab3b2d
Alfie Martin <alfie.martin@experian.com>
2023-08-16 20:06:00 +0100

    as above

--- src/app/submit-details/page.tsx
+++ src/app/submit-details/page.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Form from "./Form";
 import { getCarMakes } from "@/services/carapi";
 
-
 export default async function SubmitDetails() {
   const carMakes = await getCarMakes(global.apiKey);
 
