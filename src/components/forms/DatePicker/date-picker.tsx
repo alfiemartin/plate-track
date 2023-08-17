48284bd893727e0e0de59834899cb302872bb05f
Alfie Martin <alfie.martin@experian.com>
2023-08-18 00:36:14 +0100

    ?

--- src/components/forms/DatePicker/date-picker.tsx
+++ src/components/forms/DatePicker/date-picker.tsx
@@ -1,11 +1,11 @@
 import { InputProps, Input } from "@nextui-org/react";
-import React, { forwardRef, useState } from "react";
+import React, { forwardRef } from "react";
 import { useController, useFormContext } from "react-hook-form";
 import DatePicker from "react-datepicker";
-import { FormInputs, FormNames } from "@/app/submit-details/form";
 
 import "react-datepicker/dist/react-datepicker.css";
 import moment from "moment";
+import { FormInputs, FormNames } from "../../../app/submit-details/form";
 
 interface ControlledDatePickerProps extends InputProps {
   name: FormNames;
